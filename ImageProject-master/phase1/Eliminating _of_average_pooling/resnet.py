6e4d2939fa6fcf4efccf24c23fb4f70410998420
hojat <hojat.rd133@yahoo.com>
2023-06-25 03:09:01 +0330

    1.remove averagepooling and repair demensions(128x8192 and 512*16x10) on resnet.py 2.edit from resnet import * on main.py

--- ImageProject-master/phase1/Eliminating _of_average_pooling/resnet.py
+++ ImageProject-master/phase1/Eliminating _of_average_pooling/resnet.py
@@ -82,7 +82,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear((512*16)*block.expansion, num_classes)#(128x8192 and 512*16x10)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -98,7 +98,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        # out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
