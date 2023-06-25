de25c59b4154c503d93bc520c13705f7fe8669fe
hojat <hojat.rd133@yahoo.com>
2023-06-25 14:58:00 +0330

    add maxpool on resnet.py before layer1

--- ImageProject-master/phase1/Add_Maxpool/resnet.py
+++ ImageProject-master/phase1/Add_Maxpool/resnet.py
@@ -78,6 +78,9 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+         # ////////////////////////////////////////////////////////
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # //////////////////////////////////////////////////////////
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
