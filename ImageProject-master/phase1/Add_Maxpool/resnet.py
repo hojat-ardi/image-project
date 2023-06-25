7ce5b2ea523c7bba4c1c34530dd31f449a503b01
hojat <hojat.rd133@yahoo.com>
2023-06-25 15:21:32 +0330

    add maxpool on resnet.py before layer1

--- ImageProject-master/phase1/Add_Maxpool/resnet.py
+++ ImageProject-master/phase1/Add_Maxpool/resnet.py
@@ -78,11 +78,14 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+         # add maxpooling
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # //////////////////////////////////////////////////////////
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear((512*16)*block.expansion, num_classes)#(128x8192 and 512*16x10)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -98,7 +101,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        # out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
