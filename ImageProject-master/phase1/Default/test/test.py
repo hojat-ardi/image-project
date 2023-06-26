acb906ac7f87ef36120bace3dba163ba418992b1
hojat <hojat.rd133@yahoo.com>
2023-06-26 15:52:17 +0330

    edit test default

--- ImageProject-master/phase1/Default/test/test.py
+++ ImageProject-master/phase1/Default/test/test.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 # Please Create a folder named results in the root directory of the project for saving the results in results.log
-file_handler = logging.FileHandler('results/results.log')
+file_handler = logging.FileHandler('/content/image-project/ImageProject-master/phase1/Default/test/results.log')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
